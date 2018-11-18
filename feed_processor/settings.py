e0cd8a18e1dd426846c0ccfb67ed8000f6616066
ashhadulislam@gmail.com <ashhadulislam@gmail.com>
2018-11-18 08:19:25 +0530

    completed version 1

--- feed_processor/settings.py
+++ feed_processor/settings.py
@@ -74,16 +74,10 @@ WSGI_APPLICATION = 'feed_processor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME' : 'feed_data_db',
-        'USER' : 'root',
-        'PASSWORD' : 'secret',
-        'default-character-set' : 'utf8',
-        'HOST' : '127.0.0.1',
-        'PORT' : '3306',
-    }
+DATABASES = {   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': 'mydatabase',
+   }
 }
 
 
