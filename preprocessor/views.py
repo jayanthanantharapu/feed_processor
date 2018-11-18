b49a698dc2fa36ee586394c95839c8eabe275f13
Sanjay_PradeepKumar <Sanjay_PradeepKumar@symantec.com>
2018-11-18 09:17:37 +0530

    Removing unused variable

--- preprocessor/views.py
+++ preprocessor/views.py
@@ -11,44 +11,45 @@ from sentimentanalyser import train
 
 def training_model(request):
 	if request.method == 'GET':
-	    template = loader.get_template('preprocessor/training_data_file_upload.html')
-	    context = {}
-	    return HttpResponse(template.render(context, request))
+	    template = loader.get_template('preprocessor/training_data_file_upload.html')	    
+	    return HttpResponse(template.render({}, request))
 	elif request.method == 'POST':
 		myfile = request.FILES['labelled_file']
-		fs = FileSystemStorage()
-		fs.save(myfile.name, myfile)
+		fileSystemStorage = FileSystemStorage()
+		fileSystemStorage.save(myfile.name, myfile)
+
 		# setting labelled data to train model
 		train_labelled_set(settings.MEDIA_ROOT+'/'+myfile.name, settings.MEDIA_ROOT)
 		# render html
 		template = loader.get_template('preprocessor/testing_trained_data.html')
 	    context = {"trained_sets": os.listDir(settings.TRAINED_SETS__ROOT)}
+
 	    return HttpResponse(template.render(context, request))
 
 def testing_trained_model(request):
 	if request.method == 'GET':
 	    template = loader.get_template('preprocessor/testing_trained_data.html')
 	    context = {"trained_sets": os.listDir(settings.TRAINED_SETS__ROOT)}
+
 	    return HttpResponse(template.render(context, request))
 	elif request.method == 'POST':
 		request.POST['unlabelled_set']
 		# testing unlabelled data of train model
 		# render html
-		template = loader.get_template('preprocessor/tested_unlabelled_set_reports.html')
-	    context = {}
-	    return HttpResponse(template.render(context, request))
+		template = loader.get_template('preprocessor/tested_unlabelled_set_reports.html')	    
+	    return HttpResponse(template.render({}, request))
 
 ### Sentiment Analysis 
-
+# Create an instance of the package class.
+# Call for a method to execute and get results.
 def train_labelled_set(filename, output_dir):
 	trainObj=train.Train()
 	trainObj.train_file_model(filePath, output_dir)
 
+# Create an instance of the package class.
+# Call for a method to execute and get results.
 def test_unlabelled_set(test_filename, test_referenced_file, output_dir):
 	testObj=test.TestData()
-	testedDataFrame=testObj.test_model(
-		"",
-		test_filename,
-		test_referenced_file,
-		output_dir
-	)
+	testedDataFrame=testObj.test_model("", test_filename, test_referenced_file, output_dir)
+
+	
